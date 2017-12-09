58b5176daff265840b7e360722021572bfeea633
Veronika Laznova <laznova.veronika@gmail.com>
2017-12-09 15:57:28 +0100

    pridavam read me a licenci

--- kalkulacka/__init__.py
+++ kalkulacka/__init__.py
@@ -2,4 +2,4 @@
 
 from kalkulacka.logic import main
 
-__all__ = ['main']
+__all__ = ['main']  # verejne api, to co doporucuju sirit verejne, v souboru logic muzu mit vic funkci, ktere ale nedoporucuju pouzivat a dal sdilet (ale technicky to porad mozne je)
